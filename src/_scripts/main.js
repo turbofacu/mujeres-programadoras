87376558d59110c9f1ebffd32ae8265a2213f558
Turbofacu <turbofacu@aerolab.co>
2018-05-20 21:01:13 -0300

    header + footer stuff

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -8,22 +8,48 @@ var Swiper = require('../_scripts/swiper');
 
 var pressSwiper = new Swiper ('.press-swiper', {
   slidesPerView: 3,
-  spaceBetween: 24,
+  spaceBetween: 48,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
   breakpoints: {
     768: {
       slidesPerView: 1,
-      spaceBetween: 10
+      spaceBetween: 12,
     },
     1060: {
       slidesPerView: 2,
-      spaceBetween: 20
+      spaceBetween: 12
     },
-  }
+  },
+  navigation: {
+    nextEl: '.press-arrow-next',
+    prevEl: '.press-arrow-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
 });
 
 var quotesSwiper = new Swiper ('.quotes-swiper', {
   slidesPerView: 1,
   spaceBetween: 24,
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: '.quotes-arrow-next',
+    prevEl: '.quotes-arrow-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
 });
 
 var WOW = require('../_scripts/wow.js').WOW;
@@ -43,4 +69,19 @@ $(function() {
   );
   wow.init();
 
+});
+
+$(document).ready(function(){
+  var headerOffset = $('.list-icons-section').offset().top;
+  $(document).on('scroll', function() {
+    if($(window).scrollTop() > headerOffset) {
+      $('.header').addClass('show');
+    } else {
+      $('.header').removeClass('show');
+    }
+  });
+  $('.header-menu').on('click tap', function(){
+    $('.nav').toggleClass('show');
+  });
+  $('.counter').counterUp();
 });
